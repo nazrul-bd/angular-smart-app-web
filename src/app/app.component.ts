453dbb2aacc7a7903c677846019c45e657b2def2
nazrul <islam.nazrul@gmail.com>
2023-08-10 12:01:33 +0600

    >> Property Binding    > IMG [SRC]    > [disable] property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,13 @@ export class AppComponent {
 
   customerAddress: string = "Dhaka, Bangladesh";
   customers = [{name: "Customer1", age: 99}, {name: "Customer2", age: 88}];
+  allowTemperatureSubmitBtn : boolean = false;
+  imagePath = "../assets/river.png";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowTemperatureSubmitBtn = true;
+      console.log("Set allowTemperatureSubmitBtn TRUE")
+    }, 1000);
+  }
 }
