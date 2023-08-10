f0296c3fd4dd26893b550e0455e5d122e48bea0e
nazrul <islam.nazrul@gmail.com>
2023-08-10 10:52:06 +0600

    [NG] Understanding binding      >> https://angular.io/guide/binding-overview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles:[`
-   h3{color: red}
-  `]
+  styleUrls: ['./app.component.css']
+  // styles:[`
+  //  h3{color: red}
+  // `]
 })
 export class AppComponent {
+
+  customerAddress: string = "Dhaka, Bangladesh";
+  customers = [{name: "Customer1", age: 99}, {name: "Customer2", age: 88}];
 }
